e5e1183cb967b960f3079ff58529998ecb641d36
meraamine <63201349+meraamine@users.noreply.github.com>
2023-12-27 18:25:30 +0200

    notification

---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatefulWidget {
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  List<String> notifications = [
+    'Notification 1',
+    'Notification 2',
+    'Notification 3',
+    'Notification 4',
+    'Notification 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notifications[index]),
+          );
+        },
+      ),
+    );
+  }
+}
