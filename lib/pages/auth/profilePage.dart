c32a27dfe097b1a497fdec710773060231e06601
meraamine <63201349+meraamine@users.noreply.github.com>
2024-01-13 12:25:59 +0200

    profile

--- lib/pages/auth/profilePage.dart
+++ lib/pages/auth/profilePage.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shopify_app/models/user.model.dart';
+import 'package:shopify_app/pages/home_page.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -37,21 +38,83 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('Profile'),
-      ),
+          backgroundColor: Colors.white,
+          title: Text('Profile'),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => HomePage()));
+            },
+            icon: Icon(Icons.arrow_back),
+          )),
       body: _user != null
           ? Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListTile(
-                  title: Text('Name: ${_name ?? 'N/A'}'),
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                          width: 130,
+                          height: 130,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 4,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor),
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius: 2,
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.1),
+                                  offset: Offset(0, 10))
+                            ],
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIhsJ2D692LXEDT04mefdqZtznqnIg31FnmX5-aETP1O-rA40rRhSYbMrmMTfTVHHwoCk&usqp=CAU")),
+                          )),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                    width: 4,
+                                    color: Theme.of(context)
+                                        .scaffoldBackgroundColor),
+                                color: Colors.white),
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.black,
+                            ),
+                          ))
+                    ],
+                  ),
                 ),
                 ListTile(
-                  title: Text('Email: ${_user!.email ?? 'N/A'}'),
+                    title: Text(
+                  'Name: ${_name ?? 'N/A'}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontFamily: 'RobotoMono'),
+                )),
+                ListTile(
+                  title: Text(
+                    'Email: ${_user!.email ?? 'N/A'}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: 'RobotoMono'),
+                  ),
                 ),
                 ListTile(
-                  title: Text('Phone: ${_phone ?? 'N/A'}'),
+                  title: Text(
+                    'Phone: ${_phone ?? 'N/A'}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: 'RobotoMono'),
+                  ),
                 ),
               ],
             )
